1028bb53dbc290f4fb4efa4512a496215ec4dd62
Paulo Henrique Meneghini <paulo.meneghini@outlook.com>
2025-05-09 20:06:15 -0300

    chore: removing some comments

--- src/components/pages/audio-upload/AudioUpload.tsx
+++ src/components/pages/audio-upload/AudioUpload.tsx
@@ -83,73 +83,8 @@ export const AudioUpload = () => {
         </h1>
 
         <div className="flex flex-col lg:flex-row gap-6 mb-8">
-          {/* <div className="lg:w-1/2 w-full">
-            <h3 className="text-lg font-medium mb-2">Enviar arquivo do seu computador</h3>
-
-            <label className="block border-2 border-dashed border-textDash p-17 text-center cursor-pointer rounded-lg bg-blue-50">
-              <input type="file" accept="audio/*" multiple className="hidden" onChange={handleFileChange} />
-              <p className="text-blue-600 font-medium">Selecione ou arraste arquivos para enviar</p>
-            </label>
-          </div> */}
 
           <UploadButton func={handleFileChange}/>
-
-          {/* <div className="lg:w-1/2 w-full border rounded-sm shadow border-textDash">
-            <div className="max-h-80 overflow-auto">
-              <table className="w-full text-sm text-left">
-                <thead className="bg-blue-100">
-                  <tr>
-                    <th className="p-2">Título</th>
-                    <th className="p-2">Tamanho</th>
-                    <th className="p-2">Citação</th>
-                    <th className="p-2">Ações</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {files.length > 0 ? (
-                    files.map((file, idx) => (
-                      <tr key={idx} className="bg-dashboard">
-                        <td className="p-2">{file.name}</td>
-                        <td className="p-2">{(file.size / 1e6).toFixed(1)} MB</td>
-                        <td className="p-2">
-                          <input
-                            type="text"
-                            value={citacoes[idx]}
-                            onChange={(e) => handleCitacaoChange(idx, e.target.value)}
-                            className="border rounded-sm p-1 w-full bg-dash"
-                            placeholder="Digite a citação"
-                          />
-                        </td>
-                        <td className="p-2">
-                          {
-                            (sendingFiles && uploadStatus[idx] === 'uploading') ? (
-                              <p className="text-yellow-500">Fazendo upload...</p>
-                            ) : uploadStatus[idx] === 'done' ? (
-                              <p className="text-green-600">Enviado</p>
-                            ) : uploadStatus[idx] === 'error' ? (
-                              <p className="text-red-500">Erro no upload</p>
-                            ) : (
-                              !sendingFiles && (
-                                <button onClick={() => handleRemoveFile(idx)} className="text-red-500">
-                                  Remover
-                                </button>
-                              )
-                            )
-                          }
-                        </td>
-                      </tr>
-                    ))
-                  ) : (
-                    <tr>
-                      <td colSpan={4} className="text-center p-4 text-gray-500">
-                        Nenhum arquivo selecionado
-                      </td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
-            </div>
-          </div> */}
 
           <UploadTable 
             files={files}
