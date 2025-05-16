4b84e4f92fd577b5ac71d32f3c9bd479f7f6f497
Paulo Henrique Meneghini <paulo.meneghini@outlook.com>
2025-05-15 21:26:52 -0300

    fix: resolving font errors

--- src/components/atoms/dashboard-sidebar/Dashboard-sidebar.tsx
+++ src/components/atoms/dashboard-sidebar/Dashboard-sidebar.tsx
@@ -5,7 +5,7 @@ export const DashboardSideBar = () => {
         <aside className="w-[15rem] bg-sidebar p-4">
             <div className="flex flex-col items-center mb-4">
                 <img src="../../public/assets/images/dashboard/woman.svg" alt="Logo" className="w-32 h-32 object-contain" />
-                <h1 className="text-5xl text-center font-league font-bold tracking-wide">Nanã Chatbot</h1>
+                <h1 className="text-5xl text-center font-gothic font-bold tracking-wide">Nanã Chatbot</h1>
                 <p className="text-base text-gray-700 text-center">Dashboard Admin</p>
                 <SideBarButton />
                 <SideBarButton />
